8b880270f74711eea6d84338b2a483a5df483616
dan2xxx <160023562+LaserFi@users.noreply.github.com>
2024-03-12 22:14:00 +0200

    Update CErc20Immutable.sol

--- contracts/CErc20Immutable.sol
+++ contracts/CErc20Immutable.sol
@@ -28,7 +28,6 @@ contract CErc20Immutable is CErc20 {
                 string memory symbol_,
                 uint8 decimals_,
                 address payable admin_,
-                address BlastPointsAddressTestnet,
                 address _pointsOperator) {
         // Creator of the contract is admin during initialization
         admin = payable(msg.sender);
@@ -40,6 +39,21 @@ contract CErc20Immutable is CErc20 {
         admin = admin_;
 
         // Blast fancy
-        IBlastPoints(BlastPointsAddressTestnet).configurePointsOperator(_pointsOperator);
+        IBlastPoints(0x2fc95838c71e76ec69ff817983BFf17c710F34E0).configurePointsOperator(_pointsOperator);
+        IBlast(0x4300000000000000000000000000000000000002).configureClaimableGas(); 
+        IERC20Rebasing(0x4300000000000000000000000000000000000004).configure(YieldMode.CLAIMABLE);
+    }
+
+    /**
+     * @notice Admin only blast integration
+     * @param _receiver Treasury address
+     */
+    function _claimAllYields(address _receiver) external {
+        if (msg.sender != admin) {
+            revert ("Admin check");
+        }
+        IBlast(0x4300000000000000000000000000000000000002).claimAllGas(address(this), _receiver);
+        uint256 amount = IERC20Rebasing(0x4300000000000000000000000000000000000004).getClaimableAmount(address(this));
+        IERC20Rebasing(0x4300000000000000000000000000000000000004).claim(_receiver, amount);
     }
 }
